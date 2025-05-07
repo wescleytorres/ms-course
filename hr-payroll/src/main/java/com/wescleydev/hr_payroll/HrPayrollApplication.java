838fec4b7622dfb5e82c0baf7070a04f0e9e1ff5
WescleyUA <jewescley@gmail.com>
2025-05-07 18:24:02 -0300

    feat: implemented initial loadbalance ribbon

--- hr-payroll/src/main/java/com/wescleydev/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/wescleydev/hr_payroll/HrPayrollApplication.java
@@ -3,9 +3,11 @@ package com.wescleydev.hr_payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableFeignClients
 @SpringBootApplication
+@RibbonClient(name = "hr-worker")
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
